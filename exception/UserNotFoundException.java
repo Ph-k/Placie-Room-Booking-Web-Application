583606f23ea27d3737896012bb7e137d4a03dfbf
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-07-28 16:26:38 +0300

    Initial Commit

---
+++ exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(Long id) {
+        super("Couldn't find User having id = " + id);
+    }
+}
