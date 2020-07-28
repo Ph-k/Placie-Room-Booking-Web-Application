39515bec934d37dcf1339ea4d0a0d9dbe60f34d8
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-07-28 15:31:10 +0300

    Delete AdministratorRepository.java

--- repository/AdministratorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Administrator;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
-}
