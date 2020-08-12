a8330fa8a68b37ddf73ee6670c39ac8eddb573b0
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-12 16:42:29 +0300

    /Registration doesn't need authorising

--- SpringBoot/java-com.example.demo/security/WebSecurity.java
+++ SpringBoot/java-com.example.demo/security/WebSecurity.java
@@ -29,7 +29,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests().antMatchers("/Registration").permitAll().
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
