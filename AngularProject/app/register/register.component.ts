20ab1bc9e4abd352c9659574bab9005d66a23d11
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-13 16:32:02 +0300

    Update register.component.ts

--- AngularProject/app/register/register.component.ts
+++ AngularProject/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../service/user.service';
+import {User} from '../../model/User';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {}
+  constructor(public userService: UserService) {}
+
+  user: User;
+  passwordVerification = '';       // variable  used for password verification
+  attemptedRegistration = false;   // When 'register' button is clicked it becomes true
 
   ngOnInit(): void {
+    this.user = {userName: '', password: '', telephone: '', firstName: '', photoUrl: '', email: '', lastName: '',
+      isHost: false, isTenant: false, isAdmin: false , userId: null};
+  }
+
+  register(): void{
+    this.attemptedRegistration = true;
+    if (!this.emptyFields() && this.passwordMatch()) {
+      this.userService.register(this.user);
+    }
+  }
+
+  successfulRegistration(): boolean{
+    if (this.userService.registerResponse != null && this.userService.registerResponse != ''){
+      return true;
+    }
+
+    else{
+      return false;
+    }
+  }
+
+  usernameExists(): boolean{
+    if (this.userService.registerResponse == null && this.attemptedRegistration){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  passwordMatch(): boolean {
+    if (this.user.password == this.passwordVerification) {return true; }
+    else {return false; }
+  }
+
+  emptyFields(): boolean{
+    if (this.user.userName == '' || this.user.password == '' || this.user.telephone == '' || this.user.firstName == '' ||
+        this.user.email == '' || this.user.lastName == ''){
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
