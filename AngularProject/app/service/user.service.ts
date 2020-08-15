2d814c883e8257030cae92dba2ffee9836ce63e3
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-15 09:17:55 +0300

    Update user.service.ts

--- AngularProject/app/service/user.service.ts
+++ AngularProject/app/service/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../../model/User';
+import {PendingHost} from '../../model/PendingHost';
 import {Observable} from 'rxjs';
 
 @Injectable({
