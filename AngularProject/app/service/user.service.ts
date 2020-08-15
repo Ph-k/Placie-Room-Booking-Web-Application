446e5fef3e988e6bcd2ef6b7e91a15df776c24df
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-15 09:10:16 +0300

    Update user.service.ts

--- AngularProject/app/service/user.service.ts
+++ AngularProject/app/service/user.service.ts
@@ -7,15 +7,42 @@ import {User} from '../../model/User';
 })
 export class UserService {
 
-  private registrationUrl = 'https://localhost:8443/Registration';
 
-  registerResponse = ''; // Used to receive the response when registering a new user
+  private registrationUrl = 'https://localhost:8443/Registration';
+  private usersUrl = 'https://localhost:8443/Users';
+  private pendingHostsUrl = 'https://localhost:8443/PendingHosts';
+
+  registerResponse: User; // Used to receive the response when registering a new user
 
   constructor(private http: HttpClient) {
   }
 
-  register(user: User): void {
-    this.http.post<any>(this.registrationUrl, user).subscribe(response => {this.registerResponse = response; });
+  register(user: User, host: boolean): void {
+    this.http.post<User>(this.registrationUrl, user).subscribe(response => {this.registerResponse = response; console.log(this.registerResponse); if (host){this.uploadPendingHost(this.registerResponse.userId); }});
+  }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  getUser(id: string): Observable<User>{
+    return this.http.get<User>(this.usersUrl + '/' + id);
+  }
+
+  updateUser(user: User, id: number): void{
+    this.http.put<any>(this.usersUrl + '/118', user).subscribe();
+  }
+
+  getPendingHost(id: string): Observable<PendingHost>{
+    return this.http.get<PendingHost>(this.pendingHostsUrl + '/' + id);
+  }
+
+  uploadPendingHost(pendingHost: number): void{
+    this.http.post<any>(this.pendingHostsUrl, pendingHost).subscribe();
+  }
+
+  deletePendingHost(pendingHost: number): void{
+    this.http.delete('https://localhost:8443/PendingHosts/118').subscribe();
   }
 
 }
