51cffc889e956d316a032679bb4ec86fa8884141
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-22 16:29:20 +0300

    Update users.component.ts

--- AngularProject/app/administrator/users/users.component.ts
+++ AngularProject/app/administrator/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../service/user.service';
 import {User} from '../../../model/User';
+import {PendingHost} from '../../../model/PendingHost';
 
 @Component({
   selector: 'app-users',
@@ -12,9 +13,21 @@ export class UsersComponent implements OnInit {
   constructor(private userService: UserService ) { }
 
   users: User[];
+  pendingHosts: PendingHost[] = [];
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(users => this.users = users);
+    this.userService.getPendingHosts().subscribe(pendingHosts => {this.pendingHosts = pendingHosts; });
   }
+
+  isPendingHost(id: number): boolean{
+    for (let i = 0; i < this.pendingHosts.length; i = i + 1) {
+      if (this.pendingHosts[i].userId === id){
+        return true;
+      }
+    }
+    return false;
+  }
+
 
 }
