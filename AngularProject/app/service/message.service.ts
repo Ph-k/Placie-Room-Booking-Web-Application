3d746862a647bdc9f16cd7c00d247e5d802805ea
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-22 13:50:30 +0300

    http requests now with tokens

--- AngularProject/app/service/message.service.ts
+++ AngularProject/app/service/message.service.ts
@@ -11,18 +11,22 @@ export class MessageService {
 
   private SendMessageUrl = 'https://localhost:8443/Messages';
   private MessagesUrl = 'https://localhost:8443';
-  constructor(private http: HttpClient) { }
+  private authorizationHeader: { headers: { Authorization: string } } ;
+
+  constructor(private http: HttpClient) {
+    this.authorizationHeader = { headers: {Authorization: localStorage.getItem('token') }  };
+  }
 
   SendMessage(message: Message): Observable<Message> {
-    return this.http.post<Message>(this.SendMessageUrl, message);
+    return this.http.post<Message>(this.SendMessageUrl, message, this.authorizationHeader);
   }
 
   getContactedUsers(userID: number): Observable<string[]>{
-    return this.http.get<string[]>(this.MessagesUrl + '/ContactedUsers/' + userID);
+    return this.http.get<string[]>(this.MessagesUrl + '/ContactedUsers/' + userID, this.authorizationHeader);
   }
 
   getMessagesBetween(userID1: number, userID2: number): Observable<Message[]>{
-    return this.http.get<Message[]>(this.MessagesUrl + '/MessagesBetween/' + userID1 + '/' + userID2);
+    return this.http.get<Message[]>(this.MessagesUrl + '/MessagesBetween/' + userID1 + '/' + userID2, this.authorizationHeader);
   }
 
 }
