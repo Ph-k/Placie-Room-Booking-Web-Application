724e7133179a7486e548fa993b6db527d9621774
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-29 08:44:44 +0300

    Get Place/{id} accessible by everyone

--- SpringBoot/java-com.example.demo/security/WebSecurity.java
+++ SpringBoot/java-com.example.demo/security/WebSecurity.java
@@ -36,7 +36,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/Registration","/Users/Image/{username}","/AllMessages/{ReceiverId}"
                 ,"/ContactedUsers/{ReceiverId}","/MessagesBetween/{SenderId}/{ReceiverId}"
-                ,"/Messages","/Places/Image/{placeId}","/Places/MainImage/{placeId}","/Places/PhotoRange/{placeId}","/Places/Images/{photoId}").permitAll().antMatchers(HttpMethod.GET,"/Availabilities","AvailabilitiesFor/{AvailabilityId}").permitAll().
+                ,"/Messages","/Places/Image/{placeId}","/Places/MainImage/{placeId}","/Places/PhotoRange/{placeId}","/Places/Images/{photoId}").permitAll().
+                antMatchers(HttpMethod.GET,"/Availabilities","/AvailabilitiesFor/{AvailabilityId}","/Places/{placeId}").permitAll().
                 anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
