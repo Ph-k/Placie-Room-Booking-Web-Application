8dbf452f4be5d974a2341f80d253b3200b7208d5
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-08-30 08:52:35 +0300

    All Places

--- AngularProject/app/all-places/all-places.component.ts
+++ AngularProject/app/all-places/all-places.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from '../service/user.service';
 import {PlaceService} from '../service/place.service';
 import {Place} from '../../model/Place';
 
@@ -11,11 +10,59 @@ import {Place} from '../../model/Place';
 export class AllPlacesComponent implements OnInit {
 
   places: Place[];
+  filteredPlaces: Place[];
+  numOfPlaces: number;
+  numOfPages: number;
+  currentPage: number;
+
+  minPrice = null;
+  maxPrice = null;
 
   constructor(private placeService: PlaceService) { }
 
   ngOnInit(): void {
-    this.placeService.getPlaces().subscribe(places => this.places = places);
+    this.placeService.getPlaces().subscribe(places => {
+      this.places = places;
+      this.filteredPlaces = this.places.slice();
+      this.numOfPlaces = this.places.length;
+      this.setNumOfPages();
+      this.currentPage = 1;
+      console.log(this.numOfPages);
+    });
+  }
+
+  getFilteredPlaces(): void{
+    this.filteredPlaces = [];
+    for (var i = 0;  i < this.places.length; i++) {
+      if (this.places[i].minCost >= this.minPrice && this.places[i].minCost <= this.maxPrice){
+        this.filteredPlaces.push(this.places[i]);
+      }
+    }
+    this.setNumOfPages();
+  }
+
+  setNumOfPages(): void{
+    this.numOfPages = Math.ceil(this.filteredPlaces.length / 9 );
+  }
+
+  GetImageUrl(id: number): string{
+    return this.placeService.GetImageUrl(id.toString());
+  }
+
+  nextPage(): void{
+    if (this.currentPage < this.numOfPages){
+      this.currentPage ++;
+    }
+  }
+
+  previousPage(): void{
+    if (this.currentPage > 1){
+      this.currentPage --;
+    }
+  }
+
+  setPage(i: number): void{
+    this.currentPage = i;
   }
 
 }
