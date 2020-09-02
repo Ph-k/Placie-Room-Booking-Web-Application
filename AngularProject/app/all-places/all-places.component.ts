22d7d6f2fc6ba1a4c192956697ce59e580901b5b
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-09-02 10:25:46 +0300

    Filters + Some Styling

--- AngularProject/app/all-places/all-places.component.ts
+++ AngularProject/app/all-places/all-places.component.ts
@@ -15,9 +15,24 @@ export class AllPlacesComponent implements OnInit {
   numOfPlaces: number;
   numOfPages: number;
   currentPage: number;
+  filtersShown = false;
 
   minPrice = null;
   maxPrice = null;
+  additionalCostPerPerson = null;
+  type = null;
+  minArea = null;
+  minNumOfBeds = null;
+  minNumOfSleepingRooms = null;
+  livingRoom = false;
+  wiFi = false;
+  airConditioning = false;
+  heating = false;
+  parking = false;
+  elevator = false;
+  petsAllowed = false;
+  partiesAllowed = false;
+  smokingAllowed = false;
 
   constructor(private placeService: PlaceService, private route: ActivatedRoute) { }
 
@@ -43,15 +58,96 @@ export class AllPlacesComponent implements OnInit {
     this.filteredPlaces = [];
     const length = this.places.length;
     for (let i = 0; i < length; i++) {
-      if (this.places[i].minCost >= this.minPrice && this.places[i].minCost <= this.maxPrice){
-        this.filteredPlaces.push(this.places[i]);
-      }
+      this.filteredPlaces.push(this.places[i]);
     }
+
+    if (this.minPrice != null) {
+      this.filteredPlaces = this.filteredPlaces.filter(place => place.minCost >= this.minPrice);
+    }
+
+    if (this.maxPrice != null) {
+      this.filteredPlaces = this.filteredPlaces.filter(place => place.minCost <= this.maxPrice);
+    }
+
+    if (this.type != null && this.type !== 'null') {
+      this.filteredPlaces = this.filteredPlaces.filter(place => place.type  === this.type);
+    }
+
+    if (this.additionalCostPerPerson != null) {
+      this.filteredPlaces = this.filteredPlaces.filter(place => place.additionalCostPerPerson  <= this.additionalCostPerPerson);
+    }
+
+    if (this.minArea != null){
+      this.filteredPlaces = this.filteredPlaces.filter (place => place.area >= this.minArea);
+    }
+
+    if (this.minArea != null){
+      this.filteredPlaces = this.filteredPlaces.filter (place => place.area >= this.minArea);
+    }
+
+    if (this.minNumOfBeds != null){
+      this.filteredPlaces = this.filteredPlaces.filter (place => place.numberOfBeds >= this.minNumOfBeds);
+    }
+
+    if (this.minNumOfSleepingRooms != null){
+      this.filteredPlaces = this.filteredPlaces.filter (place => place.numberOfSleepingRooms >= this.minNumOfSleepingRooms);
+    }
+
+    if (this.minNumOfSleepingRooms != null){
+      this.filteredPlaces = this.filteredPlaces.filter (place => place.numberOfSleepingRooms >= this.minNumOfSleepingRooms);
+    }
+
+    if (this.livingRoom){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.livingRoom === true);
+    }
+
+    if (this.wiFi){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.wiFi === true);
+    }
+
+    if (this.airConditioning){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.airConditioning === true);
+    }
+
+    if (this.heating){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.heating === true);
+    }
+
+    if (this.parking){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.parking === true);
+    }
+
+    if (this.elevator){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.elevator === true);
+    }
+
+    if (this.petsAllowed){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.petsAllowed === true);
+    }
+
+    if (this.partiesAllowed){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.partiesAllowed === true);
+    }
+
+    if (this.smokingAllowed){
+      this.filteredPlaces = this.filteredPlaces.filter( place => place.smokingAllowed === true);
+    }
+
+
+
     this.setNumOfPages();
   }
 
   setNumOfPages(): void{
     this.numOfPages = Math.ceil(this.filteredPlaces.length / 9 );
+  }
+
+  showFilters(): void{
+    this.filtersShown = true;
+  }
+
+  hideFilters(): void{
+    this.filtersShown = false;
   }
 
   GetImageUrl(id: number): string{
