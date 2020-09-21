9eeb58ca2bace44a18aaf508b598455b75fbe3e8
Philippos Koumparos <filippos_7@hotmail.com>
2020-09-21 18:28:47 +0300

    Deleting Gihub Non-Sense

--- sdi1800104_sdi1800083/PlacieBackend/src/main/java/com/example/demo/exception/ReviewNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class ReviewNotFoundException extends RuntimeException {
-
-    public ReviewNotFoundException(Long id) {
-        super("Could not find a review with id: " + id);
-    }
-
-}
--- sdi1800104_sdi1800083/PlacieBackend/src/main/java/com/example/demo/repository/PendingHostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.PendingHost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PendingHostRepository extends JpaRepository<PendingHost, Long> {
-}
